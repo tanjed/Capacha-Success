de18ec5b0de2366341212a48cfe121c87779e3f0
tanjed <ahmed15-6849@diu.edu.bd>
2019-10-14 02:07:15 +0600

    Getting Mouse Coordinates

--- dragndrop.js
+++ dragndrop.js
@@ -1,4 +1,5 @@
 var id, id1, id2, id3;
+var co_ordinates = [];
 
 function allowDrop(ev)
 {
@@ -8,9 +9,23 @@ function allowDrop(ev)
 function dragStart(ev)
 {
 	id = ev.target.id;
+	getPositions(id);
 }
 
 function drop(ev)
-{
+{	
 	ev.target.append(document.getElementById(id));
+	getPositions(id);
+	console.log(co_ordinates);
+
+}
+function getPositions(id){
+
+	
+	
+}
+function dragging(ev){
+	var x = event.clientX;     // Get the horizontal coordinate
+	var y = event.clientY;     // Get the vertical coordinate
+	co_ordinates.push({"x":x , "y":y});
 }
